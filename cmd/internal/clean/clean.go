qiniu/qnlang

cmd: gop go -multi-files

cmd/internal/clean/clean.go
@@ -27,6 +27,7 @@ import (
 )
 
 const (
+	autoGenFilePrefix = "gop_autogen_"
 	autoGenFileSuffix = "_autogen.go"
 	autoGenTestFile   = "gop_autogen_test.go"
 	autoGen2TestFile  = "gop_autogen2_test.go"
@@ -53,7 +54,7 @@ func cleanAGFiles(dir string, execAct bool) {
 			}
 			continue
 		}
-		if strings.HasSuffix(fname, autoGenFileSuffix) {
+		if strings.HasSuffix(fname, autoGenFileSuffix) || strings.HasPrefix(fname, autoGenFilePrefix) {
 			file := filepath.Join(dir, fname)
 			fmt.Printf("Cleaning %s ...\n", file)
 			if execAct {

gengo.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/goplus/gogen"
 	"github.com/goplus/mod/gopmod"
 	"github.com/goplus/mod/modcache"
 	"github.com/goplus/mod/modfetch"
@@ -35,6 +36,7 @@ const (
 	autoGenFile      = "gop_autogen.go"
 	autoGenTestFile  = "gop_autogen_test.go"
 	autoGen2TestFile = "gop_autogen2_test.go"
+	autoGenPrefix    = "gop_autogen_"
 )
 
 type GenFlags int
@@ -42,6 +44,7 @@ type GenFlags int
 const (
 	GenFlagCheckOnly GenFlags = 1 << iota
 	GenFlagSingleFile
+	GenFlagMultiFiles
 	GenFlagPrintError
 	GenFlagPrompt
 )
@@ -166,7 +169,7 @@ func genGoSingleFile(file string, conf *Config, flags GenFlags) (err error) {
 }
 
 func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*bool) (err error) {
-	out, test, err := LoadDir(dir, conf, genTestPkg, (flags&GenFlagPrompt) != 0)
+	out, test, err := LoadDir(dir, conf, genTestPkg, (flags&GenFlagPrompt) != 0, (flags&GenFlagMultiFiles) != 0)
 	if err != nil {
 		if NotFound(err) { // no Go+ source files
 			return nil
@@ -177,10 +180,18 @@ func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*
 		return nil
 	}
 	os.MkdirAll(dir, 0755)
-	file := filepath.Join(dir, autoGenFile)
-	err = out.WriteFile(file)
-	if err != nil {
-		return errors.NewWith(err, `out.WriteFile(file)`, -2, "(*gogen.Package).WriteFile", out, file)
+
+	if flags&GenFlagMultiFiles != 0 {
+		err = writeMultiFiles(out, dir)
+		if err != nil {
+			return errors.NewWith(err, `writeMultiFiles(out, dir)`, -2, "(*gogen.Package).WriteFile", out, dir)
+		}
+	} else {
+		file := filepath.Join(dir, autoGenFile)
+		err = out.WriteFile(file)
+		if err != nil {
+			return errors.NewWith(err, `out.WriteFile(file)`, -2, "(*gogen.Package).WriteFile", out, file)
+		}
 	}
 	if gen != nil { // say `gop_autogen.go generated`
 		*gen[0] = true
@@ -202,6 +213,29 @@ func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*
 		err = nil
 	}
 	return
+}
+
+func writeMultiFiles(pkg *gogen.Package, dir string) error {
+	names := make(map[string]string)
+	pkg.ForEachFile(func(fname string, file *gogen.File) {
+		if fname == "" {
+			names[fname] = autoGenFile
+		} else {
+			_, name := filepath.Split(fname)
+			name = name[:len(name)-len(filepath.Ext(name))]
+			if strings.HasSuffix(name, "_test") {
+				return
+			}
+			names[fname] = autoGenPrefix + name + ".go"
+		}
+	})
+	for fname, file := range names {
+		err := pkg.WriteFile(filepath.Join(dir, file), fname)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // -----------------------------------------------------------------------------

visualfc/gop

cmd/internal/clean/clean.go
@@ -27,6 +27,7 @@ import (
 )
 
 const (
+	autoGenFilePrefix   = "xgo_autogen_"
 	autoGenFileSuffix   = "_autogen.go"
 	autoGenGopTestFile  = "gop_autogen_test.go"
 	autoGen2GopTestFile = "gop_autogen2_test.go"
@@ -55,7 +56,7 @@ func cleanAGFiles(dir string, execAct bool) {
 			}
 			continue
 		}
-		if strings.HasSuffix(fname, autoGenFileSuffix) {
+		if strings.HasSuffix(fname, autoGenFileSuffix) || strings.HasPrefix(fname, autoGenFilePrefix) {
 			file := filepath.Join(dir, fname)
 			fmt.Printf("Cleaning %s ...\n", file)
 			if execAct {

tool/gengo.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/goplus/gogen"
 	"github.com/goplus/mod/modcache"
 	"github.com/goplus/mod/modfetch"
 	"github.com/goplus/mod/xgomod"
@@ -35,6 +36,7 @@ const (
 	autoGenFile      = "xgo_autogen.go"
 	autoGenTestFile  = "xgo_autogen_test.go"
 	autoGen2TestFile = "xgo_autogen2_test.go"
+	autoGenPrefix    = "xgo_autogen_"
 )
 
 type GenFlags int
@@ -42,6 +44,7 @@ type GenFlags int
 const (
 	GenFlagCheckOnly GenFlags = 1 << iota
 	GenFlagSingleFile
+	GenFlagMultiFiles
 	GenFlagPrintError
 	GenFlagPrompt
 )
@@ -166,7 +169,7 @@ func genGoSingleFile(file string, extn int, conf *Config, flags GenFlags) (err e
 }
 
 func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*bool) (err error) {
-	out, test, err := LoadDir(dir, conf, genTestPkg, (flags&GenFlagPrompt) != 0)
+	out, test, err := LoadDir(dir, conf, genTestPkg, (flags&GenFlagPrompt) != 0, (flags&GenFlagMultiFiles) != 0)
 	if err != nil {
 		if NotFound(err) { // no XGo source files
 			return nil
@@ -177,10 +180,18 @@ func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*
 		return nil
 	}
 	os.MkdirAll(dir, 0755)
-	file := filepath.Join(dir, autoGenFile)
-	err = out.WriteFile(file)
-	if err != nil {
-		return errors.NewWith(err, `out.WriteFile(file)`, -2, "(*gogen.Package).WriteFile", out, file)
+
+	if flags&GenFlagMultiFiles != 0 {
+		err = writeMultiFiles(out, dir)
+		if err != nil {
+			return errors.NewWith(err, `writeMultiFiles(out, dir)`, -2, "(*gogen.Package).WriteFile", out, dir)
+		}
+	} else {
+		file := filepath.Join(dir, autoGenFile)
+		err = out.WriteFile(file)
+		if err != nil {
+			return errors.NewWith(err, `out.WriteFile(file)`, -2, "(*gogen.Package).WriteFile", out, file)
+		}
 	}
 	if gen != nil { // say `xgo_autogen.go generated`
 		*gen[0] = true
@@ -202,6 +213,29 @@ func genGoIn(dir string, conf *Config, genTestPkg bool, flags GenFlags, gen ...*
 		err = nil
 	}
 	return
+}
+
+func writeMultiFiles(pkg *gogen.Package, dir string) error {
+	names := make(map[string]string)
+	pkg.ForEachFile(func(fname string, file *gogen.File) {
+		if fname == "" {
+			names[fname] = autoGenFile
+		} else {
+			_, name := filepath.Split(fname)
+			name = name[:len(name)-len(filepath.Ext(name))]
+			if strings.HasSuffix(name, "_test") {
+				return
+			}
+			names[fname] = autoGenPrefix + name + ".go"
+		}
+	})
+	for fname, file := range names {
+		err := pkg.WriteFile(filepath.Join(dir, file), fname)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // -----------------------------------------------------------------------------